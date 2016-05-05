1bdbc561451bc4e722c22cc3bbb4aeed697202fd
Jazz <jazzman.svi@yandex.ru>
2016-05-05 14:36:17 +0300

    Update sample.php

--- sample.php
+++ sample.php
@@ -1,154 +1,176 @@
 <?php
-    include_once __DIR__ . '/class-widget-builder.php';
-    // Check if the custom class exists
-    if ( ! class_exists('MV_My_Recent_Posts_Widget')) {
-        // Create custom widget class extending WPH_Widget
-        class MV_My_Recent_Posts_Widget extends Widget_Builder
-        {
-            public function __construct()
-            {
-                // Configure widget array
-                $args = [
-                    // Widget Backend label
-                    'label'       => __('My Recent Posts', 'mv-my-recente-posts'),
-                    // Widget Backend Description
-                    'description' => __('My Recent Posts Widget Description', 'mv-my-recente-posts'),
-                ];
-                // Configure the widget fields
-                // Example for: Title ( text ) and Amount of posts to show ( select box )
-                // fields array
-                $args['fields'] = [
-                    // Title field
-                    [
-                        // field name/label
-                        'name'  => __('Title', 'mv-my-recente-posts'),
-                        // field description
-                        'desc'  => __('Enter the widget title.', 'mv-my-recente-posts'),
-                        // field id
-                        'id'    => 'title',
-                        // field type ( text, checkbox, textarea, select, select-group )
-                        'type'  => 'text',
-                        // class, rows, cols
-                        'class' => 'widefat',
-                        // default value
-                        'std'   => __('Recent Posts', 'mv-my-recente-posts'),
-                        /*
-                            Set the field validation type/s
-                            ///////////////////////////////
+  /**
+   * Created by PhpStorm.
+   * User: jazzman
+   * Date: 14.04.16
+   * Time: 23:52
+   */
+  namespace Upages_Widgets;
 
-                            'alpha_dash'
-                            Returns FALSE if the value contains anything other than alpha-numeric characters, underscores or dashes.
+  use Upages_Objects\Widget_Builder;
 
-                            'alpha'
-                            Returns FALSE if the value contains anything other than alphabetical characters.
+  /**
+   * Class Widgets_Boxes
+   *
+   * @package Upages_Widgets
+   */
+class Widget_Boxes extends Widget_Builder
+{
+    /**
+     * @param $args
+     */
+    public $boxes_fields = [];
 
-                            'alpha_numeric'
-                            Returns FALSE if the value contains anything other than alpha-numeric characters.
-
-                            'numeric'
-                            Returns FALSE if the value contains anything other than numeric characters.
-
-                            'boolean'
-                            Returns FALSE if the value contains anything other than a boolean value ( true or false ).
-
-                            ----------
-
-                            You can define custom validation methods. Make sure to return a boolean ( TRUE/FALSE ).
-                            Example:
-
-                            'validate' => 'my_custom_validation',
-
-                            Will call for: $this->my_custom_validation( $value_to_validate );
-
-                        */
-                        'validate' => 'alpha_dash',
-                        /*
-
-                            Filter data before entering the DB
-                            //////////////////////////////////
-
-                            strip_tags ( default )
-                            wp_strip_all_tags
-                            esc_attr
-                            esc_url
-                            esc_textarea
-
-                        */
-                        'filter' => 'strip_tags|esc_attr',
-                    ],
-                    // Amount Field
-                    [
-                        'name'     => __('Amount'),
-                        'desc'     => __('Select how many posts to show.', 'mv-my-recente-posts'),
-                        'id'       => 'amount',
-                        'type'     => 'select',
-                        // selectbox fields
-                        'fields'   => [
-                            [
-                                // option name
-                                'name'  => __('1 Post', 'mv-my-recente-posts'),
-                                // option value
-                                'value' => '1',
-                            ],
-                            [
-                                'name'  => __('2 Posts', 'mv-my-recente-posts'),
-                                'value' => '2',
-                            ],
-                            [
-                                'name'  => __('3 Posts', 'mv-my-recente-posts'),
-                                'value' => '3',
-                            ],
-                            // add more options
-                        ],
-                        'validate' => 'my_custom_validation',
-                        'filter'   => 'strip_tags|esc_attr',
-                    ],
-                    // Output type checkbox
-                    [
-                        'name'   => __('Output as list', 'mv-my-recente-posts'),
-                        'desc'   => __('Wraps posts with the <li> tag.', 'mv-my-recente-posts'),
-                        'id'     => 'list',
-                        'type'   => 'checkbox',
-                        // checked by default:
-                        'std'    => 1, // 0 or 1
-                        'filter' => 'strip_tags|esc_attr',
-                    ],
-                    // add more fields
-                ]; // fields array
-                // create widget
-                $this->create_widget($args);
-            }
-
-            // Custom validation for this widget
-            public function my_custom_validation($value)
-            {
-                if (strlen($value) > 1) {
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-
-            // Output function
-            public function widget($args, $instance)
-            {
-                // And here do whatever you want
-                $out = $args['before_title'];
-                $out .= $instance['title'];
-                $out .= $args['after_title'];
-                // here you would get the most recent posts based on the selected amount: $instance['amount']
-                // Then return those posts on the $out variable ready for the output
-                $out .= '<p>Hey There! </p>';
-                echo $out;
-            }
-        } // class
-        // Register widget
-        if ( ! function_exists('mv_my_register_widget')) {
-            function mv_my_register_widget()
-            {
-                register_widget('MV_My_Recent_Posts_Widget');
-            }
-
-            add_action('widgets_init', 'mv_my_register_widget', 1);
+    /**
+     * Widgets_Boxes constructor.
+     */
+    public function __construct()
+    {
+        $this->setBoxesFields();
+        $args             = [
+        'label'       => __('Boxes', 'superlist'),
+        'description' => __('Text boxes with icons.', 'superlist'),
+        ];
+        $args['fields']   = [
+        [
+          'name' => __('Title', 'superlist'),
+          'id'   => 'title',
+          'type' => 'text',
+        ],
+        [
+          'name' => __('Description', 'superlist'),
+          'id'   => 'description',
+          'type' => 'textarea',
+        ],
+        [
+          'name' => '1.' . __('Title:', 'superlist'),
+          'type' => 'text',
+        ],
+        [
+          'name' => '1.' . __('Content:', 'superlist'),
+          'type' => 'textarea'
+        ],
+        [
+          'name' => '1.' . __('Icon Class:', 'superlist'),
+          'type' => 'text'
+        ],
+        [
+          'name'   => '1.' . __('Read More Link:', 'superlist'),
+          'type'   => 'select',
+          'fields' => $this->getPageList()
+        ],
+        [
+          'name' => '2.' . __('Title:', 'superlist'),
+          'type' => 'text',
+        ],
+        [
+          'name' => '2.' . __('Content:', 'superlist'),
+          'type' => 'textarea'
+        ],
+        [
+          'name' => '2.' . __('Icon Class:', 'superlist'),
+          'type' => 'text'
+        ],
+        [
+          'name'   => '2.' . __('Read More Link:', 'superlist'),
+          'type'   => 'select',
+          'fields' => $this->getPageList()
+        ],
+        [
+          'name' => '3.' . __('Title:', 'superlist'),
+          'type' => 'text',
+        ],
+        [
+          'name' => '3.' . __('Content:', 'superlist'),
+          'type' => 'textarea'
+        ],
+        [
+          'name' => '3.' . __('Icon Class:', 'superlist'),
+          'type' => 'text'
+        ],
+        [
+          'name'   => '3.' . __('Read More Link:', 'superlist'),
+          'type'   => 'select',
+          'fields' => $this->getPageList()
+        ],
+        ];
+        $advanced_options = $this->add_advanced_options();
+        foreach ($advanced_options as $option) {
+            $args['fields'][] = $option;
         }
+        parent::__construct($args);
     }
+
+    /**
+     * @param mixed $boxes_fields
+     */
+    public function setBoxesFields()
+    {
+        $boxes_fields_count = 3;
+        for ($i = 1; $i <= $boxes_fields_count; $i++) {
+            $this->boxes_fields[] = [
+            [
+            'name' => $i . '.' . __('Title:', 'superlist'),
+            'type' => 'text',
+            ],
+          [
+            'name' => $i . '.' . __('Content:', 'superlist'),
+            'type' => 'textarea'
+          ],
+          [
+            'name' => $i . '.' . __('Icon Class:', 'superlist'),
+            'type' => 'text'
+          ],
+          [
+            'name'   => $i . '.' . __('Read More Link:', 'superlist'),
+            'type'   => 'select',
+            'fields' => $this->getPageList()
+          ]
+            ];
+        }
+
+        return $this->boxes_fields;
+    }
+
+    /**
+     * @param array $args
+     * @param array $instance
+     */
+    public function widget($args, $instance)
+    {
+        echo wp_kses($args['before_widget'], wp_kses_allowed_html('post')); ?>
+      <div <?php echo $this->advanced_style($instance) ?>>
+        <?= $this->advanced_widget_title_and_description($instance) ?>
+        <div class="row">
+            <?php for ($i = 1; $i <= 3; $i++) : ?>
+            <?php $title_id = $i . '-nazva'; ?>
+            <?php $content_id = $i . '-vmist'; ?>
+            <?php $icon_id = $i . '-icon-class'; ?>
+            <?php $link_id = $i . '-read-more-link'; ?>
+            <div class="col-sm-4">
+              <div class="box">
+                <div class="box-icon">
+                  <i class="fa <?php echo wp_kses($instance[$icon_id], wp_kses_allowed_html('post')); ?>"></i>
+                </div>
+                <div class="box-body">
+                  <h4 class="box-title"><?php echo wp_kses($instance[$title_id], wp_kses_allowed_html('post')); ?></h4>
+                  <div class="box-content">
+                    <?php echo wp_kses($instance[$content_id], wp_kses_allowed_html('post')); ?>
+                  </div>
+                    <?php $read_more = $instance[$link_id]; ?>
+                    <?php if (! empty($read_more)) : ?>
+                    <a href="<?php echo wp_kses($read_more, wp_kses_allowed_html('post')); ?>" class="box-read-more">
+                        <?php echo esc_attr__('Read More', 'superlist'); ?>
+                      <i class="fa fa-angle-right"></i>
+                    </a>
+                    <?php endif; ?>
+                </div>
+              </div>
+            </div>
+            <?php endfor; ?>
+        </div>
+      </div>
+        <?php echo wp_kses($args['after_widget'], wp_kses_allowed_html('post'));
+    }
+
+}
